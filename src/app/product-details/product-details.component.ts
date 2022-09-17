c021e9b43b0b1b1b0aa8704936eabe19311881eb
digvijay-sin <76095132+digvijay-sin@users.noreply.github.com>
2022-09-17 12:42:03 +0530

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -18,6 +18,8 @@ export class ProductDetailsComponent implements OnInit {
 
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(product => product.id === productIdFromRoute);
   } 
 
 }
